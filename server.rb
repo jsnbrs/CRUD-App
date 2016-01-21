5fbe00e3e96062e2328a9760e4b3fd967e8d20dd
Jason Booras <jasonbooras@gmail.com>
2016-01-21 10:26:19 -0500

    add view for posts and delete for posts

--- server.rb
+++ server.rb
@@ -5,6 +5,8 @@ require 'pry'
 
 class Server < Sinatra::Base
 
+  set :method_override, true
+
   get '/' do
     erb :index
   end
@@ -15,10 +17,9 @@ class Server < Sinatra::Base
 
   post '/post' do
     db = db_connect
+
     title = params[:title]
     post = params[:post]
-    "#{title}"
-    "#{post}"
 
     @new_post = db.exec_params("INSERT INTO posts (title, post) VALUES ($1, $2)", [title, post])
 
@@ -27,13 +28,21 @@ class Server < Sinatra::Base
   end
 
 
-  get '/read/:post' do
-        "#{title}"
-    "#{post}"
+  get '/read' do
+    db = db_connect
+
+    @posts = db.exec("SELECT * FROM posts").to_a
     erb :read
   end
 
-  private
+  delete '/read/:id' do
+    db = db_connect
+
+    db.exec("DELETE FROM posts WHERE id = #{params["id"].to_i}").first
+
+    "Post removed!"
+    redirect('/read')
+  end
 
   def db_connect
     PG.connect(dbname: "forum")
