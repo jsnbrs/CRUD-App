fd5c17d492e53f3fb61cd9902f93aef608e58e09
Jason Booras <jasonbooras@gmail.com>
2016-01-20 20:17:10 -0500

    add post comment content

--- server.rb
+++ server.rb
@@ -1,16 +1,22 @@
+require "sinatra/base"
+require 'pg'
+require 'bcrypt'
+require 'pry'
+
 class Server < Sinatra::Base
 
   get '/' do
-    redirect "/index"
-  end
-
-  get '/index' do
     erb :index
   end
 
-  get '/post' do
+  get '/comment' do
     erb :post
   end
+
+  post '/comment' do
+    "Hello World"
+  end
+
 
   get '/read' do
     erb :read
